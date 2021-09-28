41b39a95bb22f8355d5268cec34740ca0035eac6
root <megaboy1129@naver.com>
2021-09-28 21:28:37 +0900

    test

--- data.tf
+++ data.tf
@@ -31,7 +31,7 @@ data "aws_eks_cluster_auth" "eks_cluster" {
 }
 
 data "template_file" "launch_template_userdata" {
-  template = file("${path.module}/templates/userdata.sh.tpl")
+  template = file(".terraform/module/eks/templates/userdata.sh.tpl")
 
   vars = {
     cluster_name        = format("${var.vpc_name}+%s", "EKS")
